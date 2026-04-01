3d8186406261c198a6d2ea076d5e60d04eddf98c
nicolasmlohrmann-bit <nicolasmlohrmann@gmail.com>
2026-03-31 23:11:11 -0300

    Add constantes.cpp to calculate circumference and constants

---
+++ constantes.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+    const double PI = 3.14159;  
+    double radio = 10;
+    double circunferencia = 2 * PI * radio;
+
+    const int VelosidadDeLuz = 299792458;
+    const int Ancho = 1920;
+    const int Alto = 1080;
+
+
+    std::cout<<"La circunferencia es: "<<circunferencia<<" centimetros"<<std::endl;      
+
+    
+
+    return 0;
+}

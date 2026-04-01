ec3511fec0b9d8cbaf01c4ca9eb377f357fdc834
nicolasmlohrmann-bit <nicolasmlohrmann@gmail.com>
2026-03-31 23:12:05 -0300

    Add type aliases for text and decimal types

---
+++ typedef_typealiases.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//typedef std::vector<std::pair<std::string, int>> pair_list_t;
+typedef std::string text_t;
+typedef int decimal_t; 
+typedef decimal_t precio_t;
+
+int main() {
+
+    precio_t precio = 2;
+    cout<<precio<<std::endl;
+
+    text_t nombre = "Nicolas";
+    cout<<nombre<<std::endl; 
+
+    return 0;
+}

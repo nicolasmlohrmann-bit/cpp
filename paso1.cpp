6cea699f529992dacc5918a0e4599ed0788eaa2d
nicolasmlohrmann-bit <nicolasmlohrmann@gmail.com>
2026-03-31 23:10:06 -0300

    Add simple C++ program to print greetings
    Initial implementation of a simple C++ program that prints greetings to the console.

---
+++ paso1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() 
+{
+    std::cout << "Hola Mundo" << std::endl;
+    std::cout << "Nose Que Poner xd"<<'\n';
+    std::cout << "Hola Otra Ves";
+    return 0;
+}
+/*
+    *Entonces para hacer un salto de linea se utilisa el
+    << std::endl;
+    << '\n';
+*/  
